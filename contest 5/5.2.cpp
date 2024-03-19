12ca59dc4a7130119e68cdf5585c09bef308e4fc
AsyaXiao <yfcnz2005.smirnova@yandex.ru>
2024-03-19 22:08:52 +0300

    contest 5

---
+++ contest 5/5.2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+int main() {
+    {
+        std::ofstream myfile;//поток вывода файла
+        myfile.open("../data/input.txt");
+        if (myfile.is_open()) {
+            myfile << "4 3" << "\n";
+            myfile << "sweden 4 6 3" << "\n";
+            myfile << "queen 0 2 2 \n";
+            myfile << "norway 4 7 5 \n";
+            myfile << "king -1 3 2 \n";
+        }
+
+        myfile.close();
+    }
+    std::ifstream myfile("../data/input.txt");
+    int N, M;
+    if (myfile.is_open()) {
+        myfile >> M >> N;
+        std::vector<std::string> words;
+        std::vector<int> summa;
+        std::vector<int> for_1;
+        std::string word1;
+        myfile >> word1;
+        for (size_t i = 0; i < N; ++i) {
+            int a;
+            myfile >> a;
+            for_1.push_back(a);
+        }
+        for (size_t m = 0; m < M - 1; ++m) {
+            std::string a;
+            myfile >> a;
+            words.push_back(a);
+            int summa1 = 0;
+            for (size_t n = 0; n < N; ++n) {
+                int b;
+                myfile >> b;
+                summa1 += b * for_1[n];
+            }
+            summa.push_back(summa1);
+        }
+        int closest;
+        closest = *std::max_element(summa.begin(), summa.end());
+        for (size_t k = 0; k < summa.size(); ++k) {
+            if (summa[k] == closest) {
+                std::cout << words[k] << std::endl;
+            }
+        }
+    }
+}
--- contest 5/5.3.cpp
+++ contest 5/5.3.cpp
@@ -4,91 +4,76 @@
 #include <iostream>
 #include <cmath>
 
-class Complex
-{
+class Complex {
     double _re;
     double _im;
 
 public:
-    Complex(double re, double im)
-    {
+    Complex(double re, double im) {
         _re = re;
         _im = im;
     }
 
-    Complex(double re)
-    {
+    Complex(double re) {
         _re = re;
         _im = 0;
     }
 
-    bool operator==(const Complex& rhs) const
-    {
+    bool operator==(const Complex &rhs) const {
         return rhs.Re() == Re() && rhs.Im() == Im();
     }
 
-    bool operator!=(const Complex& rhs) const
-    {
+    bool operator!=(const Complex &rhs) const {
         return !(*this == rhs);
     }
 
-    Complex operator+(const Complex& rhs) const
-    {
+    Complex operator+(const Complex &rhs) const {
         return Complex(rhs.Re() + Re(), rhs.Im() + Im());
     }
 
-    Complex operator-(const Complex& rhs) const
-    {
+    Complex operator-(const Complex &rhs) const {
         return Complex(Re() - rhs.Re(), Im() - rhs.Im());
     }
 
-    Complex operator*(const Complex& rhs) const
-    {
+    Complex operator*(const Complex &rhs) const {
         double re = Re() * rhs.Re() - Im() * rhs.Im();
         double im = Re() * rhs.Im() + Im() * rhs.Re();
         return Complex(re, im);
     }
 
-    Complex operator/(const Complex& rhs) const
-    {
+    Complex operator/(const Complex &rhs) const {
         double denominator = rhs.Re() * rhs.Re() + rhs.Im() * rhs.Im();
         double re = (Re() * rhs.Re() + Im() * rhs.Im()) / denominator;
         double im = (Im() * rhs.Re() - Re() * rhs.Im()) / denominator;
         return Complex(re, im);
     }
 
-    Complex operator-() const
-    {
+    Complex operator-() const {
         return Complex(-_re, -_im);
     }
 
-    Complex operator+() const
-    {
+    Complex operator+() const {
         return *this;
     }
 
-    double Re() const
-    {
+    double Re() const {
         return _re;
     }
 
-    double Im() const
-    {
+    double Im() const {
         return _im;
     }
 };
 
-Complex operator*(const Complex& lhs, const double& rhs)
-{
+Complex operator*(const Complex &lhs, const double &rhs) {
     return Complex(lhs.Re() * rhs, lhs.Im() * rhs);
 }
 
-Complex operator*(const double& lhs, const Complex& rhs)
-{
+Complex operator*(const double &lhs, const Complex &rhs) {
     return rhs * lhs;
 }
 
-double abs(const Complex& a)
-{
+double abs(const Complex &a) {
     return sqrt(a.Re() * a.Re() + a.Im() * a.Im());
 }
+
